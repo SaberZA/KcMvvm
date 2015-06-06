e80b9f54bc9a01bbb6a46a425cd4118ececd472f
Steven van der Merwe <stevenvdmerwe@gmail.com>
2015-06-06 11:17:34 +0200

    Added iOS and Android with Mvvm support

---
+++ KcMvvm.Droid/obj/Debug/android/src/md5562ea8ea88bbdf203826d1ab511d79b5/SplashScreen.java
@@ -0,0 +1,37 @@
+package md5562ea8ea88bbdf203826d1ab511d79b5;
+
+
+public class SplashScreen
+	extends md57b8dd31b26d57b878589ceca204f3b49.MvxSplashScreenActivity
+	implements
+		mono.android.IGCUserPeer
+{
+	static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("KcMvvm.Droid.SplashScreen, KcMvvm.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", SplashScreen.class, __md_methods);
+	}
+
+
+	public SplashScreen () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == SplashScreen.class)
+			mono.android.TypeManager.Activate ("KcMvvm.Droid.SplashScreen, KcMvvm.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+	java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ iOS/Views/FirstView.cs
@@ -0,0 +1,36 @@
+using Cirrious.MvvmCross.Binding.BindingContext;
+using Cirrious.MvvmCross.Touch.Views;
+using CoreGraphics;
+using Foundation;
+using ObjCRuntime;
+using UIKit;
+using Core = KcMvvm.Core;
+
+namespace iOS.Views
+{
+    [Register("FirstView")]
+    public class FirstView : MvxViewController
+    {
+        public override void ViewDidLoad()
+        {
+            View = new UIView { BackgroundColor = UIColor.White };
+            base.ViewDidLoad();
+
+			// ios7 layout
+            if (RespondsToSelector(new Selector("edgesForExtendedLayout")))
+            {
+               EdgesForExtendedLayout = UIRectEdge.None;
+            }
+			   
+            var label = new UILabel(new CGRect(10, 10, 300, 40));
+            Add(label);
+            var textField = new UITextField(new CGRect(10, 50, 300, 40));
+            Add(textField);
+
+            var set = this.CreateBindingSet<FirstView, Core.ViewModels.FirstViewModel>();
+            set.Bind(label).To(vm => vm.Hello);
+            set.Bind(textField).To(vm => vm.Hello);
+            set.Apply();
+        }
+    }
+}
