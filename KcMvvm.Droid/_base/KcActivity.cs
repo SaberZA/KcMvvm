e6527179f818430fe106ee8b4067462aece891f8
Steven van der Merwe <stevenvandermerwe@Stevens-MacBook-Air.local>
2015-06-08 11:00:04 +0200

    Fixed missing refs

---
+++ KcMvvm.Droid/_base/KcActivity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Cirrious.MvvmCross.Droid.Views;
+using KcMvvm.Core;
+using Android.Widget;
+
+namespace KcMvvm.Droid
+{
+	public class KcActivity<T> : MvxActivity<T>
+		where T: KcViewModel
+	{
+		public KcActivity ()
+		{
+		}
+
+		public void ShowToastMessage(string message, ToastLength duration)
+		{
+			RunOnUiThread (() => Toast.MakeText(this,message,duration).Show());
+		}
+	}
+}
+
